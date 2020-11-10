24cc5c7f4bb55dd702d7abd0b96783678451355c
Kirill <kirill.morozov_1994@mail.ru>
2020-11-10 20:34:51 +0300

    Updates

---
+++ precache-manifest.d7fec188fbcdbebcc2dda5348f54bea9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b802214bec03d300d9c41a36031016d6",
+    "url": "/avia-sales/index.html"
+  },
+  {
+    "revision": "ecef6835dce6ef5b5b23",
+    "url": "/avia-sales/static/css/3.80e028c5.chunk.css"
+  },
+  {
+    "revision": "1deb916f611e6da25be3",
+    "url": "/avia-sales/static/css/main.8f3db1f7.chunk.css"
+  },
+  {
+    "revision": "a8617f2cc8298f000337",
+    "url": "/avia-sales/static/js/2.f19b7722.chunk.js"
+  },
+  {
+    "revision": "b3e2770e6336acd821638b70a2a55e3e",
+    "url": "/avia-sales/static/js/2.f19b7722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecef6835dce6ef5b5b23",
+    "url": "/avia-sales/static/js/3.291a7b30.chunk.js"
+  },
+  {
+    "revision": "391fd64c79ff68e2d424",
+    "url": "/avia-sales/static/js/4.76ce7de6.chunk.js"
+  },
+  {
+    "revision": "1deb916f611e6da25be3",
+    "url": "/avia-sales/static/js/main.a4872aad.chunk.js"
+  },
+  {
+    "revision": "42e81bd614dcd9a06216",
+    "url": "/avia-sales/static/js/runtime-main.2acf2492.js"
+  }
+]);
